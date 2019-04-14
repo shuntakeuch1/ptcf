aff10ebffd5d43954039a13390c15f09f25485bd
shuntakeuch1 <takeuchishun89@gmail.com>
2019-04-15 04:40:51 +0900

    add : map class

--- src/main.go
+++ src/main.go
@@ -5,10 +5,12 @@ import (
     "flag"
     "os"
     "bufio"
+    "strings"
 )
 var (
     f = flag.String("f", "example.puml", "file flag")
     l = flag.String("l", "php", "language flag")
+    format = flag.String("format", "default", "format flag")
 )
 func getFile() string {
     // 該当のpumlを受け取る
@@ -36,10 +38,43 @@ func readFile(file string){
     defer fp.Close()
 
     scanner := bufio.NewScanner(fp)
+    all := map[string][]string{}
+    var package_slice []string
+    var class_slice []string
     for scanner.Scan(){
-        fmt.Println(scanner.Text())
+        record := scanner.Text()
+        if strings.Contains(record,"package"){
+            r := strings.NewReader(record)
+            var (
+                title string
+                word string
+            )
+            n,err := fmt.Fscanf(r,"%s %s",&title,&word)
+            if err != nil {
+                fmt.Println(n)
+            }
+            package_slice = append(package_slice,word)
+            // fmt.Println(word)
+            if len(package_slice) != 1{
+                all[package_slice[len(package_slice) - 1]] = class_slice
+            }
+            class_slice = nil // 初期化
+        }else if strings.Contains(record,"class"){
+            r := strings.NewReader(record)
+            var (
+                title string
+                word string
+            )
+            n,err := fmt.Fscanf(r,"%s %s",&title,&word)
+            if err != nil {
+                fmt.Println(n)
+            }
+            class_slice = append(class_slice,word)
+        }
     }
-
+    // fmt.Println(package_slice)
+    // fmt.Println(class_slice)
+    fmt.Println(all)
     // 失敗読み込み業の出力
     if err = scanner.Err(); err != nil {
         fmt.Fprintf(os.Stdout,"Can not read line:%s \n", file)
