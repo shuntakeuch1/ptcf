d340e9929c71003558c3d12cd33d504f1416042d
shuntakeuch1 <takeuchishun89@gmail.com>
2019-04-15 02:08:00 +0900

    add: read file

--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "flag"
     "os"
+    "bufio"
 )
 var (
     f = flag.String("f", "example.puml", "file flag")
@@ -17,13 +18,32 @@ func getFile() string {
 func checkLanguage(lang string) bool {
     // 第2引数を受け取って対応言語か判定
     if lang == "php" {
-        return true        
+        return true
     }
     return false
 }
 
-func pumlConversionJson() {    
+func pumlConversionJson() {
     // pumlをjsonに変換
+}
+
+func readFile(file string){
+    // ファイル読み込み処理
+    fp, err := os.Open(file)
+    if err != nil {
+        fmt.Fprintf(os.Stdout,"Can not read file:%s \n", file)
+    }
+    defer fp.Close()
+
+    scanner := bufio.NewScanner(fp)
+    for scanner.Scan(){
+        fmt.Println(scanner.Text())
+    }
+
+    // 失敗読み込み業の出力
+    if err = scanner.Err(); err != nil {
+        fmt.Fprintf(os.Stdout,"Can not read line:%s \n", file)
+    }
 }
 
 func main(){
@@ -31,9 +51,9 @@ func main(){
     file := getFile()
     if !checkLanguage(*l) {
         fmt.Fprintf(os.Stdout,"This language is not supported :%s \n", *l)
-        return 
+        return
     }
-
+    readFile(file)
     // mkdir -p みたいに再帰的なディレクトリの作成 ログ出力をする
 
     // 初期フォーマット + ファイル名でファイルに記述
@@ -41,5 +61,4 @@ func main(){
     // 終了コードをいれる
     // q,r := div(19, 7)
     // fmt.Printf("商=%d 剰余=%d\n", q,r)
-    fmt.Println(file)
 }
